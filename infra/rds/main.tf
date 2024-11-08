b81e01262006d11bd74db08fe61038185833396f
jithu-09 <66712946+jithu-09@users.noreply.github.com>
2024-11-08 13:58:19 +0530

    Update main.tf

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -16,8 +16,8 @@ resource "aws_db_instance" "default" {
   allocated_storage       = 10
   storage_type            = "gp2"
   engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t2.micro"
+  engine_version          = "5.7.33"   # or another version that works with db.t2.micro
+  instance_class          = "db.t3.micro"   # Try using db.t3.micro
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
@@ -29,4 +29,5 @@ resource "aws_db_instance" "default" {
   backup_retention_period = 0
   deletion_protection     = false
 }
+
 
