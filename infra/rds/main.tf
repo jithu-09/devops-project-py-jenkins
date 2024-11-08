ee15513b6780fb43d6ba9ea712e292f9191a77bd
jithu-09 <66712946+jithu-09@users.noreply.github.com>
2024-11-08 14:09:53 +0530

    Update main.tf

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "default" {
   storage_type            = "gp2"
   engine                  = "mysql"
   engine_version          = "5.7.44"  # Use the available version 5.7.44
-  instance_class          = "db.t2.micro"  # Or try db.t3.micro if needed
+  instance_class          = "db.t3.micro"  # Or try db.t3.micro if needed
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
